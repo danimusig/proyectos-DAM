b9e16501b8137643ff22b090334e2b283e5619ed
danimusig <danimusig.dev@gmail.com>
2026-09-25 10:07:30 +0200

    Actividad 1.1 de psp

---
+++ DAM-2/programacion-servicios-y-procesos/Unidad 1/Java/App/src/App.java
@@ -0,0 +1,19 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            Process proceso = new ProcessBuilder("python", "/home/danielms/Proyects/DAM-2/programacion-servicios-y-procesos/Unidad 1/Python/proceso_python.py").start();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
+            int exitStatus = proceso.waitFor();;
+            System.out.println("Retorno:" + br.readLine());
+            System.out.println("Valor de la salida:" + exitStatus);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
